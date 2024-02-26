e303625a95bafaf479473632fd696f1e2e409316
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-02-26 17:33:11 +0100

    final

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(cookieParser()); //to read cookies and bring tokens
 app.use(
   cors({
-    origin: ["https://airbnb-clone-cfkm.vercel.app"],
+    origin: [""],
+    methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
 );

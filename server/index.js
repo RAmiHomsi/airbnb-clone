d948758fbaf6a47dc484c1ec7378744bef416c26
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-02-26 15:59:15 +0100

    fixed cookies

--- server/index.js
+++ server/index.js
@@ -107,6 +107,7 @@ app.post("/login", async (req, res) => {
           } else {
             res
               .cookie("token", token, {
+                httpOnly: true,
                 secure: process.env.NODE_ENV !== "development",
                 sameSite: "strict",
                 maxAge: 30 * 24 * 60 * 60 * 1000,
